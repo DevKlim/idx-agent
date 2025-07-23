fc39533fe2bf0479915d6bd9180fda696eee772b
Klim <klimentlamh@gmail.com>
2025-07-22 21:20:17 -0700

    sentinelai sync

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     api_host: str = "0.0.0.0"
     api_port: int = 8001
-    eido_agent_url: str = "http://eido-agent-api:8000"
+    eido_agent_url: str = "http://eido_api:8000"
 
 settings = Settings()
